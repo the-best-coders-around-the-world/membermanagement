08caad498f11f696f4a0a2709a50a074bafc61f4
KimHoDong <tunys123@gmail.com>
2024-07-10 22:39:40 +0900

    set minus

--- java/src/main/java/Calculator.java
+++ java/src/main/java/Calculator.java
@@ -8,12 +8,6 @@ public class Calculator {
 
     }
     public void minusTwoNumber(int a , int b) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("계산할 첫번째 수를 입력하세요 : ");
-        a = sc.nextInt();
-        System.out.println("계산할 두번째 수를 입력하세요 : ");
-        b = sc.nextInt();
-        int sum = a-b;
-        System.out.println(a + " - " + b " = " +sum);
+        System.out.println("a - b = " + (a - b) );
     }
 }
