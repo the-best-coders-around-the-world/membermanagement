c55b3a00855e8563d72337e85879ee990638007e
KimHoDong <tunys123@gmail.com>
2024-07-10 22:37:05 +0900

    change add minus

--- java/src/main/java/Calculator.java
+++ java/src/main/java/Calculator.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Calculator {
 
     public void divideTwoNumbers(int a, int b) {
         System.out.println("a / b = " + a / b);
+
+
+    }
+    public void minusTwoNumber(int a , int b) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("계산할 첫번째 수를 입력하세요 : ");
+        a = sc.nextInt();
+        System.out.println("계산할 두번째 수를 입력하세요 : ");
+        b = sc.nextInt();
+        int sum = a-b;
+        System.out.println(a + " - " + b " = " +sum);
     }
 }
