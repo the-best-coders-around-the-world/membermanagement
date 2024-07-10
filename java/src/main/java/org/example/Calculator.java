28ef6cdfaf608fcd5d78e2a05ac223fc226aac68
gustmd9146 <gustmd9146@gmail.com>
2024-07-10 23:50:29 +0900

    main 클래스 삭제 sumTwoNumber 메소드 추가 Calculator 클래스 example 폴더로 이동

--- java/src/main/java/org/example/Calculator.java
+++ java/src/main/java/org/example/Calculator.java
@@ -1,13 +1,18 @@
-import java.util.Scanner;
+package org.example;
 
 public class Calculator {
 
-    public void divideTwoNumbers(int a, int b) {
+    public void divideTwoNumber(int a, int b) {
         System.out.println("a / b = " + a / b);
 
 
     }
     public void minusTwoNumber(int a , int b) {
         System.out.println("a - b = " + (a - b) );
+    }
+
+    public int sumTwoNumber(int a , int b) {
+
+        return a + b;
     }
 }
